eea6381abf1bd46dad46b7cd3f0bd1e9763a2fdb
obewas <obewas1202@gmail.com>
2021-03-22 00:30:07 +0300

    moved .gitignore

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -40,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'eshop',
 ]
 
 MIDDLEWARE = [
@@ -110,7 +111,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
